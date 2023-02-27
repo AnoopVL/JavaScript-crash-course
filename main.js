07aa6b770fa72163cfafb22d3956e50f2c5c217c
AnoopVL <avlanjekar4@gmail.com>
2023-02-27 22:41:22 +0530

    varibles in js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("We are in main.js rn");
+console.log("We are in main.js rn")
+
+//--------------------------- varibles ---------------------------
+
+let age = 20
+console.log("age = " + age)
+age = 21
+console.log("new age = " + age)
+
+const value = 10
+//value  = 40; -----------this is not allowed
+console.log(value)
