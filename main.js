8e0e7d6c8b0712a8095020b9786d56ca83f4037e
AnoopVL <avlanjekar4@gmail.com>
2023-02-27 23:05:33 +0530

    data types

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("We are in main.js rn")
 
-//--------------------------- varibles ---------------------------
+/*--------------------------- varibles ---------------------------*/
 
 let age = 20
 console.log("age = " + age)
@@ -10,3 +10,30 @@ console.log("new age = " + age)
 const value = 10
 //value  = 40; -----------this is not allowed
 console.log(value)
+
+/*--------------------------- Date Types ---------------------------*/
+//strings
+const name = "Anoop"
+const name1 = "Zeno"
+//int and float
+const int = 21
+const float = 6.9
+//boolean
+const statement = true
+//undefined
+let result
+console.log(result)
+//null
+const something = null
+//complex data types
+const person = {
+  fname: "Anoop",
+  lname: "Lanjekar",
+  age: 20,
+}
+console.log(person.fname)
+const rnumbers = [21, 34, 56, 90, 09]
+console.log(rnumbers.length - 1)
+console.log(rnumbers[rnumbers.length - 1])
+
+/*--------------------------- Operators ---------------------------*/
