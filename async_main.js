4e24dccd44b242e9255ce58a2e81eabb46520903
AnoopVL <avlanjekar4@gmail.com>
2023-03-05 16:59:33 +0530

    callback functions

--- async_main.js
+++ async_main.js
@@ -1,13 +1,13 @@
 /*--------------------------- timeouts and intervals ---------------------------*/
-function greet() {
-  console.log("hello after 5 seconds")
-}
-setTimeout(greet, 5000)
+// function greet() {
+//   console.log("hello after 5 seconds")
+// }
+// setTimeout(greet, 5000)
 
-function greet1(name) {
-  console.log("hello " + name)
-}
-setTimeout(greet1, 4000, "Anoop")
+// function greet1(name) {
+//   console.log("hello " + name)
+// }
+// setTimeout(greet1, 4000, "Anoop")
 
 // function greet2(name) {
 //   console.log("Hello " + name)
@@ -15,3 +15,24 @@ setTimeout(greet1, 4000, "Anoop")
 // setInterval(greet2, 2000, "Anoop")
 // const intervalId = setInterval(greet2, 2000, "Anoop")
 // clearInterval(intervalId)
+
+/*--------------------------- callback function ---------------------------*/
+function greet(name) {
+  console.log("Hello " + name)
+}
+function greetAnoop(greetfn) {
+  const name = "Anoop"
+  greetfn(name)
+}
+greetAnoop(greet)
+
+function greet(name) {
+  console.log("Hello " + name)
+}
+function higherOrderFunction(callback) {
+  const name = "Zeno"
+  callback(name)
+}
+higherOrderFunction(greet)
+
+/*--------------------------- Promises ---------------------------*/
